0cddd4172dc81628f8e51681e5b97930148881e9
Shailesh_18_ <153095920+Shailesh2302@users.noreply.github.com>
2025-05-07 11:16:04 +0530

    Update server.js typo fixed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
   origin: [
       'https://dodcor-project-admin.vercel.app',
       'https://dodcor-project-client.vercel.app'
-    ];
+    ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'aToken'],
